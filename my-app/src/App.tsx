e0683bcccc50e82fb2219d4aa2953370daada137
ZoeNguyen2810 <miumiu2810201@gmail.com>
2024-12-08 23:42:27 +0700

    1st commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import FormPremium from './Component/formPremium.tsx';
+
+function App() {
+  return (
+    <div className="App">
+      <FormPremium />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Component/formPremium.tsx
@@ -0,0 +1,185 @@
+import React , { useState } from 'react';
+import FormControl from '@mui/material/FormControl';
+import Input from '@mui/material/Input';
+import { FormLabel, FormControlLabel, RadioGroup, Radio, MenuItem, InputLabel, Button } from '@mui/material';
+import './formPremium.css'; // Import file CSS
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+function FormPremium() {
+    const [age, setAge] = React.useState('');
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        position: '',
+        buyingService: 'No',
+        centreName: '',
+        centreAddress: '',
+        suburb: '',
+        postcode: '',
+        state: '',
+        centreEmail: '',
+        centrePhone: '',
+        goLiveDate: '',
+        openingTime: '',
+        closingTime: '',
+        approvedPlaces: '',
+        additionalInfo: '',
+        signature: '',
+    });
+
+    
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { id, value } = event.target;
+        setFormData((prevData) => ({ ...prevData, [id]: value }));
+    };
+
+    const handleRadioChange = (value: string) => {
+        setFormData((prevData) => ({ ...prevData, buyingService: value }));
+    };
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+
+        // Check for required fields
+        // const requiredFields = [
+        //     'firstName', 'email', 'position', 'centreName', 'centreAddress', 'suburb', 
+        //     'postcode', 'state', 'centreEmail', 'centrePhone', 'goLiveDate', 
+        //     'openingTime', 'closingTime', 'approvedPlaces', 'additionalInfo', 'signature'
+        // ];
+
+        // for (const field of requiredFields) {
+        //     if (!formData[field]) {
+        //         alert(`Please fill in the required field: ${field.replace(/([A-Z])/g, ' $1').toUpperCase()}`);
+        //         return;
+        //     }
+        // }
+
+        // If validation passes, submit the form
+        console.log('Zoe Form submitted with data:', formData);
+    };
+    return (
+        <FormControl className="form-control-container" style={{ marginLeft : '40%'}}>
+            {/* First Name */}
+            <div className="form-group">
+                <FormLabel htmlFor="firstName">First Name *</FormLabel>
+                <Input id="firstName" aria-describedby="first-name-helper-text" className='w350'  onChange={handleInputChange} />
+            </div>
+
+            {/* Last Name */}
+            <div className="form-group">
+                <FormLabel htmlFor="lastName">Last Name</FormLabel>
+                <Input id="lastName" aria-describedby="last-name-helper-text" className='w350' onChange={handleInputChange} />
+            </div>
+
+            {/* Email */}
+            <div className="form-group">
+                <FormLabel htmlFor="email">Your Email *</FormLabel>
+                <Input id="email" aria-describedby="email-helper-text" className='w350' onChange={handleInputChange} />
+            </div>
+
+            {/* Position */}
+            <div className="form-group">
+                <FormLabel htmlFor="position">Your Position/Role *</FormLabel>
+                <Input id="position" aria-describedby="position-helper-text" className='w350' onChange={handleInputChange} />
+            </div>
+
+            {/* Radio Group */}
+            <div>
+                <FormLabel id="demo-radio-buttons-group-label">
+                    Are you buying an existing OWNA service?
+                </FormLabel>
+                <RadioGroup
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    defaultValue="No"
+                    name="radio-buttons-group"
+                    className="radio-group"
+                    
+                >
+                  <FormControlLabel value="Yes" control={<Radio />} label="Yes" onChange={() => handleRadioChange('Yes')} />
+                  <FormControlLabel value="No" control={<Radio />} label="No" onChange={() => handleRadioChange('No')} />
+                </RadioGroup>
+
+            </div>
+            <div className="form-group">
+                <FormLabel htmlFor="centreName">Centre/Service Name * </FormLabel>
+                <Input id="centreName" aria-describedby="position-helper-text" className='w350' onChange={handleInputChange} />
+            </div>
+            <div className="form-group">
+                <FormLabel htmlFor="centreAddress">Centre/Service Physical Address *</FormLabel>
+                <Input id="centreAddress" aria-describedby="position-helper-text" className='w350' onChange={handleInputChange} />
+            </div>
+
+            <div className="form-group">
+                <FormLabel htmlFor="suburb">
+                    Suburb *</FormLabel>
+                <Input id="suburb" aria-describedby="position-helper-text" className='w350' onChange={handleInputChange} />
+            </div>
+            <div className="form-group">
+                <FormLabel htmlFor="postcode">
+                    Centre/Service Postcode  *</FormLabel>
+                <Input id="postcode" aria-describedby="position-helper-text" className='w350' onChange={handleInputChange} />
+            </div>
+            <div className="form-group">
+                <FormLabel htmlFor="state">
+                    State *</FormLabel>
+                {/* <InputLabel id="demo-simple-select-label">State</InputLabel> */}
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="state"
+                    value={age}
+                    label="Age"
+                    onChange={(e) => handleRadioChange(e.target.value)}
+                >
+                    <MenuItem value={'NSW'}>NSW</MenuItem>
+                    <MenuItem value={'ACT'}>ACT</MenuItem>
+                    <MenuItem value={'WA'}>WA</MenuItem>
+                </Select>
+            </div>
+            <div className="form-group">
+                <FormLabel htmlFor="centreEmail">
+                    Centre/Service Email Address *</FormLabel>
+                <Input id="centreEmail" aria-describedby="position-helper-text" className='w350'  onChange={handleInputChange} />
+            </div>
+            <div className="form-group">
+                <FormLabel htmlFor="centrePhone">
+                    Centre/Service Phone Number *</FormLabel>
+                <Input id="centrePhone" aria-describedby="position-helper-text" className='w350' onChange={handleInputChange}/>
+            </div>
+            <div className="form-group">
+                <FormLabel htmlFor="goLiveDate">
+                    Intended Go Live Date  *</FormLabel>
+                <Input type="date" id="goLiveDate" name="birthday" className='w350'/>
+            </div>
+            <div className="form-group">
+                <FormLabel htmlFor="openingTime">
+                    Centre/Service Opening Time  *</FormLabel>
+                <Input type='time' id="openingTime" aria-describedby="position-helper-text" className='w350' onChange={handleInputChange}/>
+            </div>
+            <div className="form-group">
+                <FormLabel htmlFor="closingTime">
+                    Closing Time  *</FormLabel>
+                <Input id="closingTime" type='time' aria-describedby="position-helper-text" className='w350' onChange={handleInputChange}/>
+            </div>
+            <div className="form-group">
+                <FormLabel htmlFor="approvedPlaces">
+                    Centre/Service Approved Places *</FormLabel>
+                <Input id="approvedPlaces" aria-describedby="position-helper-text" className='w350' onChange={handleInputChange}/>
+            </div>
+            <div className="form-group">
+                <FormLabel htmlFor="additionalInfo">
+                    Upload Additional Information *</FormLabel>
+                <Input id="additionalInfo" aria-describedby="position-helper-text" className='w350' onChange={handleInputChange}/>
+            </div>
+            <div className="form-group">
+                <FormLabel htmlFor="signature">
+                    Your Signature* *</FormLabel>
+                <Input id="signature" aria-describedby="position-helper-text"  onChange={handleInputChange}/>
+            </div>
+            <Button type="submit" style={{ backgroundColor : 'blue' , color : 'white'}} onClick={handleSubmit}>Submit</Button>
+        </FormControl>
+    );
+}
+
+export default FormPremium;
